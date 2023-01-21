743753e112a979ba6961f4a1aa4d1ecbacae6a9b
Sean Killeen <SeanKilleen@gmail.com>
2023-01-21 15:04:16 -0500

    Add SqlConnection (tests pass)

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.SqlClient;
+
 namespace TestProject1;
 
 public class Tests
@@ -10,6 +12,7 @@ public class Tests
     [Test]
     public void Test1()
     {
+        var c = new SqlConnection();
         Assert.Pass();
     }
 }
